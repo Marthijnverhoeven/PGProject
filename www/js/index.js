da160fdf764fee8d1b3c93ce1c58e2a76b8985bf
Marthijn Verhoeven <mmamhver@student.avans.nl>
2016-03-11 10:25:33 +0100

    test

--- www/js/index.js
+++ www/js/index.js
@@ -48,4 +48,25 @@ var app = {
     }
 };
 
+function doGeo()
+{
+    navigator.geolocation.getCurrentPosition(onSuccess, onFailure);
+}
+
+function onSuccess(position) {
+    console.log(position.coords.accuracy);
+    var HTML = position.coords.accuracy;
+
+    document.getElementById("geo").innerHTML = HTML;
+
+};
+
+function onFailure(error) {
+    console.log(error.message);
+
+    var HTML = error.message;
+
+    document.getElementById("geo").innerHTML = HTML;
+};
+
 app.initialize();
